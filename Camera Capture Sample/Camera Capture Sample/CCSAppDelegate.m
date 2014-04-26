73674710ee374f6e371cc2e1b6b2464197d7f0c2
Sazameki <info@sazameki.jp>
2014-04-27 03:59:48 +0900

    目が閉じているかどうかを確認して色分け表示する機能を追加した。

--- Camera Capture Sample/Camera Capture Sample/CCSAppDelegate.m
+++ Camera Capture Sample/Camera Capture Sample/CCSAppDelegate.m
@@ -11,6 +11,9 @@
 #import "ImageAndVideoSupport.h"
 
 
+#define kEyeHistoryCount    10
+
+
 @interface CCSAppDelegate ()<AVCaptureVideoDataOutputSampleBufferDelegate>
 
 @property (unsafe_unretained) IBOutlet NSWindow *mainWindow;
@@ -29,6 +32,14 @@
     CALayer                     *testLayer;
 
     CIDetector                  *faceDetector;
+
+    int     leftEyeHistory[kEyeHistoryCount];
+    int     leftEyeHistoryPos;
+    BOOL    isLeftEyeClosed;
+
+    int     rightEyeHistory[kEyeHistoryCount];
+    int     rightEyeHistoryPos;
+    BOOL    isRightEyeClosed;
 }
 
 /*!
@@ -128,6 +139,15 @@
  */
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    for (int i = 0; i < kEyeHistoryCount; i++) {
+        leftEyeHistory[i] = 0;
+        rightEyeHistory[i] = 0;
+    }
+    leftEyeHistoryPos = 0;
+    isLeftEyeClosed = NO;
+    rightEyeHistoryPos = 0;
+    isRightEyeClosed = NO;
+
     // ウィンドウサイズの設定（GUIエディタで変更しても良いが、手動でやる方が簡単。）
     [self setupWindowForWidth:720];
 
@@ -156,31 +176,112 @@
         CGContextStrokeRect(bitmapContext, faceFeature.bounds);
 
         // 口の位置の描画
-        CGPoint mouthPos = faceFeature.mouthPosition;
-        CGRect mouthRect = CGRectMake(mouthPos.x-20, mouthPos.y-20, 40, 40);
-        CGContextSetRGBFillColor(bitmapContext, 1.0, 0.0, 0.0, 0.5);
-        CGContextFillEllipseInRect(bitmapContext, mouthRect);
+        if (faceFeature.hasMouthPosition) {
+            CGPoint mouthPos = faceFeature.mouthPosition;
+            CGRect mouthRect = CGRectMake(mouthPos.x-20, mouthPos.y-20, 40, 40);
+            CGContextSetRGBFillColor(bitmapContext, 1.0, 0.0, 0.0, 0.5);
+            CGContextFillEllipseInRect(bitmapContext, mouthRect);
+        }
 
         // 左目
-        CGPoint leftEyePos = faceFeature.leftEyePosition;
-        CGRect leftEyeRect = CGRectMake(leftEyePos.x-20, leftEyePos.y-20, 40, 40);
-        if (faceFeature.leftEyeClosed) {
-            CGContextSetRGBFillColor(bitmapContext, 0.0, 0.0, 1.0, 0.5);
-        } else {
-            CGContextSetRGBFillColor(bitmapContext, 1.0, 0.0, 0.0, 0.5);
+        if (faceFeature.hasLeftEyePosition) {
+            CGPoint leftEyePos = faceFeature.leftEyePosition;
+            CGRect leftEyeRect = CGRectMake(leftEyePos.x-20, leftEyePos.y-20, 40, 40);
+            if (isLeftEyeClosed) {
+                CGContextSetRGBFillColor(bitmapContext, 0.0, 0.0, 1.0, 0.5);
+            } else {
+                CGContextSetRGBFillColor(bitmapContext, 1.0, 0.0, 0.0, 0.5);
+            }
+            CGContextFillEllipseInRect(bitmapContext, leftEyeRect);
         }
-        CGContextFillEllipseInRect(bitmapContext, leftEyeRect);
 
         // 右目
-        CGPoint rightEyePos = faceFeature.rightEyePosition;
-        CGRect rightEyeRect = CGRectMake(rightEyePos.x-20, rightEyePos.y-20, 40, 40);
-        if (faceFeature.rightEyeClosed) {
-            CGContextSetRGBFillColor(bitmapContext, 0.0, 0.0, 1.0, 0.5);
-        } else {
-            CGContextSetRGBFillColor(bitmapContext, 1.0, 0.0, 0.0, 0.5);
+        if (faceFeature.hasRightEyePosition) {
+            CGPoint rightEyePos = faceFeature.rightEyePosition;
+            CGRect rightEyeRect = CGRectMake(rightEyePos.x-20, rightEyePos.y-20, 40, 40);
+            if (isRightEyeClosed) {
+                CGContextSetRGBFillColor(bitmapContext, 0.0, 0.0, 1.0, 0.5);
+            } else {
+                CGContextSetRGBFillColor(bitmapContext, 1.0, 0.0, 0.0, 0.5);
+            }
+            CGContextFillEllipseInRect(bitmapContext, rightEyeRect);
         }
-        CGContextFillEllipseInRect(bitmapContext, rightEyeRect);
     }
+}
+
+- (BOOL)checkEyeClosedInRect:(CGRect)rect ciImage:(CIImage *)ciImage history:(int[])history historyPos:(int *)historyPos
+{
+    {
+        CIFilter *filter = [CIFilter filterWithName:@"CICrop"];
+        [filter setValue:[CIVector vectorWithX:rect.origin.x Y:rect.origin.y
+                                             Z:rect.size.width W:rect.size.height] forKey:@"inputRectangle"];
+        [filter setValue:ciImage forKey:@"inputImage"];
+        ciImage = [filter valueForKey:@"outputImage"];
+    }
+    {
+        CIFilter *filter = [CIFilter filterWithName:@"CIRowAverage"];
+        [filter setDefaults];
+        [filter setValue:ciImage forKey:@"inputImage"];
+        [filter setValue:[CIVector vectorWithCGRect:ciImage.extent] forKey:@"inputExtent"];
+        ciImage = [filter valueForKey:@"outputImage"];
+    }
+    {
+        CIFilter *filter = [CIFilter filterWithName:@"CIAreaAverage"];
+        [filter setDefaults];
+        [filter setValue:ciImage forKey:@"inputImage"];
+        [filter setValue:[CIVector vectorWithCGRect:ciImage.extent] forKey:@"inputExtent"];
+        ciImage = [filter valueForKey:@"outputImage"];
+    }
+
+    CGImageRef cgImage = SZ_CGImageCreateFromCIImage(ciImage);
+    NSData *averageData = SZ_CGImageCreateBitmapDataFromCGImage(cgImage);
+    uint8_t *buffer = (uint8_t *)averageData.bytes;
+    int r = (int)buffer[1];
+    int g = (int)buffer[2];
+    int b = (int)buffer[3];
+
+    int v = (int)(0.29891 * r + 0.58661 * g + 0.11448 * b);
+    history[(*historyPos)++] = v;
+    if (*historyPos >= kEyeHistoryCount) {
+        *historyPos = 0;
+    }
+
+    float average = 0.0f;
+    for (int i = 0; i < kEyeHistoryCount; i++) {
+        average += history[i];
+    }
+    average /= kEyeHistoryCount;
+    BOOL isEyeClosed = YES;
+    if (average < 120) {
+        isEyeClosed = NO;
+    }
+    NSLog(@"average=%.2f", average);
+    CGImageRelease(cgImage);
+
+    return isEyeClosed;
+}
+
+- (void)checkEyesClosedWithFeatures:(NSArray *)features ciImage:(CIImage *)ciImage
+{
+    if (features.count == 0) {
+        return;
+    }
+
+    CIFaceFeature *feature = features[0];
+    if (!feature.hasLeftEyePosition) {
+        return;
+    }
+
+    CGRect faceBounds = feature.bounds;
+    CGFloat eyeWidth = CGRectGetWidth(faceBounds) * 0.04;
+    CGFloat eyeHeight = CGRectGetWidth(faceBounds) * 0.1;
+    CGPoint leftEyePos = feature.leftEyePosition;
+    CGRect leftEyeRect = CGRectMake(leftEyePos.x-eyeWidth/2, leftEyePos.y-eyeHeight/2, eyeWidth, eyeHeight);
+    isLeftEyeClosed = [self checkEyeClosedInRect:leftEyeRect ciImage:ciImage history:leftEyeHistory historyPos:&leftEyeHistoryPos];
+
+    CGPoint rightEyePos = feature.rightEyePosition;
+    CGRect rightEyeRect = CGRectMake(rightEyePos.x-eyeWidth/2, rightEyePos.y-eyeHeight/2, eyeWidth, eyeHeight);
+    isRightEyeClosed = [self checkEyeClosedInRect:rightEyeRect ciImage:ciImage history:rightEyeHistory historyPos:&rightEyeHistoryPos];
 }
 
 /*!
@@ -194,6 +295,11 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
         // サンプルバッファから画像を取得
         CIImage *ciImage = SZ_CIImageCreateFromCMSampleBuffer(sampleBuffer);
 
+        CIFilter *toneFilter = [CIFilter filterWithName:@"CIFaceBalance"];
+        [toneFilter setDefaults];
+        [toneFilter setValue:ciImage forKey:@"inputImage"];
+        ciImage = [toneFilter valueForKey:@"outputImage"];
+
         // サンプルバッファから画像を取得
         CGImageRef cgImage = SZ_CGImageCreateFromCIImage(ciImage);
         CGSize imageSize = SZ_CGImageGetSize(cgImage);
@@ -202,6 +308,7 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
 
         // Core Imageフィルタで画像処理
         NSArray *features = [faceDetector featuresInImage:ciImage options:nil];
+        [self checkEyesClosedWithFeatures:features ciImage:ciImage];
         [self drawFaceFeatures:features inBitmapContext:bitmapContext];
 
         // 画像処理結果を表示
